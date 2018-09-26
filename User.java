27fcb7d951f80b7560738ffc18a95ae3b90e65c0
malekcheikh <malekcheikh8@gmail.com>
2018-09-26 10:29:23 +0100

    adding user class

---
+++ User.java
@@ -0,0 +1,34 @@
+package Jenkins.JenkinsJavaGettingStarted;
+
+public class User {
+String Id;
+String name;
+String LastName;
+public String getId() {
+	return Id;
+}
+public void setId(String id) {
+	Id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getLastName() {
+	return LastName;
+}
+public void setLastName(String lastName) {
+	LastName = lastName;
+}
+public User(String id, String name, String lastName) {
+	super();
+	Id = id;
+	this.name = name;
+	LastName = lastName;
+}
+
+
+
+}
